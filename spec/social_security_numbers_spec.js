071058fafe59c9755a6cdf880f7b376b11b00e7b
georgebabayan <ggbabayan@gmail.com>
2018-10-11 09:55:32 +0300

    adds has

--- spec/social_security_numbers_spec.js
+++ spec/social_security_numbers_spec.js
@@ -1,20 +1,20 @@
 
 
 describe(' hasSsn', () => {
-  test('reurns true if it finds an SSN', () => expect(has_ssn("The number is 234-60-1422")).toBe(true));
-  test('returns false if it does not find an SSN', () => expect(has_ssn("The number is XXX-XX-1422")).toBe(false));
+  test('reurns true if it finds an SSN', () => expect(hasSsn("The number is 234-60-1422")).toBe(true));
+  test('returns false if it does not find an SSN', () => expect(hasSsn("The number is XXX-XX-1422")).toBe(false));
 });
 
 describe('grab_ssn', () => {
-  let ssn = grab_ssn('The number is 234-60-1422')
+  let ssn = grabSsn('The number is 234-60-1422')
   test('grab number 234-60-1422', () => expect(ssn).toEqual('234-60-1422')
 
-  ssn = grab_ssn('The numbers are 350-80-0744, 234-60-1422, and 013-60-8762')
+  ssn = grabSsn('The numbers are 350-80-0744, 234-60-1422, and 013-60-8762')
   test('grabSsn 2/2', () => expect(ssn).toEqual('350-80-0744')
 });
 
 describe('returns the first SSN it finds', () => {
-  let ssn = grab_ssn('The numbers are 350-80-0744, 234-60-1422, and 013-60-8762')
+  let ssn = grabSsn('The numbers are 350-80-0744, 234-60-1422, and 013-60-8762')
   test('returns false if it does not find an SSN', () => expect(has_ssn("The number is XXX-XX-1422")).toEqual("350-80-0744"));
 
 });
